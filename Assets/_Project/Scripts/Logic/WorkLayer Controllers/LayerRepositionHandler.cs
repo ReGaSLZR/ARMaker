855436cd5a96fb1d0d0da2e136d1c25ada2524b5
Ren Salazar <salazarrenelie@gmail.com>
2025-05-04 16:06:23 +0800

    [PROTOTYPE] Implemented the WorkLayer customizations for Reposition and Rotation

---
+++ Assets/_Project/Scripts/Logic/WorkLayer Controllers/LayerRepositionHandler.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class LayerRepositionHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    [Header("Visual Settings")]
+    public Color selectionColor = Color.blue;
+    public float cornerDotSize = 0.1f;
+    public float lineThickness = 0.05f;
+    public GameObject dotPrefab; // Assign a prefab with a SpriteRenderer
+
+    private Camera mainCamera;
+    private Transform selectedTransform;
+    private LineRenderer lineRenderer;
+    private List<GameObject> cornerDots = new List<GameObject>();
+    private Vector3 offset;
+    private bool dragging;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        GameObject lineObj = new GameObject("SelectionBox");
+        lineRenderer = lineObj.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 5;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startWidth = lineThickness;
+        lineRenderer.endWidth = lineThickness;
+        lineRenderer.startColor = selectionColor;
+        lineRenderer.endColor = selectionColor;
+        lineRenderer.loop = false;
+        lineRenderer.enabled = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        TrySelectAtScreenPoint(eventData.position);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (selectedTransform != null)
+        {
+            Vector3 inputPos = eventData.position;
+            offset = selectedTransform.position - ScreenToWorld(inputPos, selectedTransform.position.z);
+            dragging = true;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (dragging && selectedTransform != null)
+        {
+            Vector3 newPos = ScreenToWorld(eventData.position, selectedTransform.position.z) + offset;
+            newPos.z = selectedTransform.position.z;
+            selectedTransform.position = newPos;
+            UpdateVisuals();
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragging = false;
+    }
+
+    private void TrySelectAtScreenPoint(Vector2 screenPoint)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(screenPoint);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            SpriteRenderer sr = hit.collider.GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                Select(sr.transform);
+            }
+        }
+    }
+
+    private void Select(Transform target)
+    {
+        selectedTransform = target;
+        UpdateVisuals();
+    }
+
+    private void UpdateVisuals()
+    {
+        if (selectedTransform == null) return;
+
+        Bounds bounds = selectedTransform.GetComponent<SpriteRenderer>().bounds;
+        Vector3[] corners = new Vector3[5]
+        {
+            bounds.min,
+            new Vector3(bounds.max.x, bounds.min.y, bounds.min.z),
+            bounds.max,
+            new Vector3(bounds.min.x, bounds.max.y, bounds.max.z),
+            bounds.min
+        };
+
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = corners.Length;
+        lineRenderer.startWidth = lineThickness;
+        lineRenderer.endWidth = lineThickness;
+        lineRenderer.startColor = selectionColor;
+        lineRenderer.endColor = selectionColor;
+        lineRenderer.SetPositions(corners);
+
+        foreach (GameObject dot in cornerDots)
+            Destroy(dot);
+        cornerDots.Clear();
+
+        if (dotPrefab != null)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                GameObject dot = Instantiate(dotPrefab, corners[i], Quaternion.identity);
+                dot.transform.localScale = Vector3.one * cornerDotSize;
+                dot.GetComponent<SpriteRenderer>().color = selectionColor;
+                cornerDots.Add(dot);
+            }
+        }
+    }
+
+    private Vector3 ScreenToWorld(Vector3 screenPos, float z)
+    {
+        screenPos.z = z - mainCamera.transform.position.z;
+        return mainCamera.ScreenToWorldPoint(screenPos);
+    }
+}
---
+++ Assets/_Project/Scripts/Logic/WorkLayer Controllers/LayerRotationHandler.cs
@@ -0,0 +1,207 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class LayerRotationHandler : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler, IPointerClickHandler
+{
+    public float boxColliderSizeMultiplierOnSelection = 2f;
+
+    public float circleRadius = 1f;
+    public float circleThickness = 0.02f;
+    public float dragSensitivity = 1f;
+
+    public Color xAxisColor = Color.red;
+    public Color yAxisColor = Color.green;
+    public Color zAxisColor = Color.blue;
+
+    private GameObject selectedSprite;
+    private RotationCircle xCircle, yCircle, zCircle;
+    private Camera mainCam;
+
+    private BoxCollider boxCollider;
+    private Vector3 originalColliderSize;
+
+    private RotationCircle activeCircle = null;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        boxCollider = GetComponent<BoxCollider>();
+        originalColliderSize = boxCollider.size;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        boxCollider.size = originalColliderSize * boxColliderSizeMultiplierOnSelection;
+        SelectSprite(gameObject);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        boxCollider.size = originalColliderSize * boxColliderSizeMultiplierOnSelection;
+        SelectSprite(gameObject);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DetectCircleHit(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 delta = eventData.delta * dragSensitivity;
+        float angleDelta = delta.x + delta.y;
+
+        switch (activeCircle.axis)
+        {
+            case Axis.X:
+                selectedSprite.transform.Rotate(Vector3.right, angleDelta, Space.World);
+                break;
+            case Axis.Y:
+                selectedSprite.transform.Rotate(Vector3.up, angleDelta, Space.World);
+                break;
+            case Axis.Z:
+            default:
+                selectedSprite.transform.Rotate(Vector3.forward, angleDelta, Space.Self);
+                break;
+        }
+
+        UpdateCircles();
+    }
+
+    void SelectSprite(GameObject sprite)
+    {
+        ClearCircles();
+        selectedSprite = sprite;
+
+        CreateCircles();
+    }
+
+    [ContextMenu("Deselect")]
+    public void Deselect()
+    {
+        boxCollider.size = originalColliderSize;
+        ClearCircles();
+    }
+
+    void CreateCircles()
+    {
+        xCircle = RotationCircle.Create(Axis.X, xAxisColor, circleRadius, circleThickness);
+        yCircle = RotationCircle.Create(Axis.Y, yAxisColor, circleRadius, circleThickness);
+        zCircle = RotationCircle.Create(Axis.Z, zAxisColor, circleRadius, circleThickness);
+
+        UpdateCircles();
+    }
+
+    void UpdateCircles()
+    {
+        Vector3 pos = selectedSprite.transform.position;
+        xCircle.UpdatePosition(pos, selectedSprite.transform);
+        yCircle.UpdatePosition(pos, selectedSprite.transform);
+        zCircle.UpdatePosition(pos, selectedSprite.transform);
+    }
+
+    void ClearCircles()
+    {
+        if (xCircle != null) Destroy(xCircle.gameObject);
+        if (yCircle != null) Destroy(yCircle.gameObject);
+        if (zCircle != null) Destroy(zCircle.gameObject);
+
+        xCircle = null;
+        yCircle = null;
+        zCircle = null;
+    }
+
+    void DetectCircleHit(Vector2 screenPos)
+    {
+        activeCircle = null;
+
+        float bestDistance = float.MaxValue;
+        foreach (var circle in new[] { xCircle, yCircle })
+        {
+            float distance = circle.DistanceToScreen(screenPos, mainCam);
+            if (distance < 30f && distance < bestDistance)
+            {
+                bestDistance = distance;
+                activeCircle = circle;
+            }
+        }
+
+        // If no X or Y circle is hit, default to Z-axis rotation
+        if (activeCircle == null)
+        {
+            activeCircle = zCircle;
+        }
+    }
+
+    public enum Axis { X, Y, Z }
+
+    class RotationCircle : MonoBehaviour
+    {
+        public Axis axis;
+        private LineRenderer lr;
+        private int segments = 64;
+        private float radius;
+        private float thickness;
+
+        public static RotationCircle Create(Axis axis, Color color, float radius, float thickness)
+        {
+            var obj = new GameObject($"RotationCircle_{axis}");
+            var circle = obj.AddComponent<RotationCircle>();
+            circle.axis = axis;
+            circle.radius = radius;
+            circle.thickness = thickness;
+
+            var lr = obj.AddComponent<LineRenderer>();
+            circle.lr = lr;
+            lr.useWorldSpace = false;
+            lr.loop = true;
+            lr.positionCount = circle.segments;
+            lr.material = new Material(Shader.Find("Sprites/Default"));
+            lr.startColor = lr.endColor = color;
+            lr.startWidth = lr.endWidth = thickness;
+            lr.sortingOrder = 100;
+
+            return circle;
+        }
+
+        public void UpdatePosition(Vector3 worldPos, Transform spriteTransform)
+        {
+            transform.position = worldPos;
+
+            Quaternion rotation = axis switch
+            {
+                Axis.X => Quaternion.Euler(0, 90, 0),
+                Axis.Y => Quaternion.Euler(90, 0, 0),
+                Axis.Z => Quaternion.identity,
+                _ => Quaternion.identity,
+            };
+            transform.rotation = rotation;
+
+            Vector3[] points = new Vector3[segments];
+            for (int i = 0; i < segments; i++)
+            {
+                float angle = 2 * Mathf.PI * i / segments;
+                points[i] = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+            }
+
+            lr.SetPositions(points);
+        }
+
+        public float DistanceToScreen(Vector2 screenPos, Camera cam)
+        {
+            float closestDist = float.MaxValue;
+
+            for (int i = 0; i < lr.positionCount; i++)
+            {
+                Vector3 worldPoint = transform.TransformPoint(lr.GetPosition(i));
+                Vector2 screenPoint = cam.WorldToScreenPoint(worldPoint);
+                float dist = Vector2.Distance(screenPos, screenPoint);
+                if (dist < closestDist) closestDist = dist;
+            }
+
+            return closestDist;
+        }
+    }
+}
