eed0252ecf2f62b25741d90a171768611439ac0c
Dean Ezekiel Salazar <deanezekiel.salazar@gmail.com>
2025-05-18 15:07:41 +0800

    [ADD] Video Player test

---
+++ Assets/_Project/Scripts/_Prototype/VideoPlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (videoPlayer.isPlaying)
+            {
+                Stop();
+            }
+            else
+            {
+                Play();
+            }
+        }
+    }
+
+    [ContextMenu("Play")]
+    public void Play()
+    {
+        videoPlayer.Play();
+    }
+
+    [ContextMenu("Stop")]
+    public void Stop()
+    {
+        videoPlayer.Stop();
+    }
+}
